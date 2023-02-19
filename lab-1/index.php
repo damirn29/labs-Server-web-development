663f7b5bd780077a41b69f140498cda92cd2cd81
Damir Nureev <damirn29@gmail.com>
2023-02-20 00:45:59 +0300

    lab-1

---
+++ lab-1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="box" style="display:flex">
+            <img src="img\Logo_Polytech_rus_main.jpg" alt="logo" width="300" style="margin-right:400px">
+            <h1>2.1.Домашняя работа: Hello, World! </h1>
+        </div>
+    </header>
+    <main>
+    <?php
+         echo 'Hello, world!';
+    ?>
+    </main>
+    <footer></footer>
+</body>
+</html>
