9c904594d3426a4ec42b378b1d9e19bf5b26c577
Damir Nureev <damirn29@gmail.com>
2023-03-06 15:12:04 +0300

    add lab-2

--- lab-1/index.php
+++ lab-1/index.php
@@ -1,23 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
-    <header>
-        <div class="box" style="display:flex">
-            <img src="img\Logo_Polytech_rus_main.jpg" alt="logo" width="300" style="margin-right:400px">
-            <h1>2.1.Домашняя работа: Hello, World! </h1>
-        </div>
-    </header>
-    <main>
     <?php
-         echo 'Hello, world!';
+
+    $array = array(
+        'Left',
+        'Middle',
+        'Right',
+    );
+
+    for ($i = 0; $i <= 2; $i++){
+        echo "<button type='button' class='btn btn-primary'>$array[$i]</button>";
+    }
+    
+
     ?>
-    </main>
-    <footer></footer>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
